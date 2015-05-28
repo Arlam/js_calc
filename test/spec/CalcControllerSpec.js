a72fed95690d017e7f2be7b99724a8b46e48987c
Arlam <arlam.ua@gmail.com>
2015-05-29 01:03:21 +0300

    fixed test

--- test/spec/CalcControllerSpec.js
+++ test/spec/CalcControllerSpec.js
@@ -2,7 +2,7 @@ describe('CalcController', function() {
 	
 	describe('Test setDigit', function() {
 		it('Should return 8 than 81', function() {
-			expect(8).toBe(Number(setDigit(8));
+			expect(8).toBe(Number(setDigit(8)));
 			expect(81).toBe(Number(setDigit(1)));
 		});
 	});
